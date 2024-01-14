3d6ac7bf41610a5605af6ecf16fd155ee51db5d0
Subhrajoti Chakrabarty <148115965+Subhrajoti@users.noreply.github.com>
2024-01-14 18:03:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-console.log("Hello Vistors");
-alert("Hello Vistors");
+console.log("Hello Every Vistors");
+// small JavaScript Project:
+var counter = document.getElementById("count");
+var decreaseButton = document.getElementById("decreaseButton");
+var increaseButton = document.getElementById("increaseButton");
+var resetButton = document.getElementById("resetButton");
+
+decreaseButton.addEventListener("click", function chageFont() {
+    var count = counter.innerText;
+    count = parseInt(count) - 1;
+    counter.textContent = count;
+})
+
+increaseButton.addEventListener("click", function onIncrease() {
+    var count = counter.innerText;
+    count = parseInt(count) + 1;
+    counter.innerText = count;
+})
+
+resetButton.addEventListener("click", function onReset() {
+    var count = counter.innerText;
+    count = 0;
+    counter.innerText = "0";
+})
