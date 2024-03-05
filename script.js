6dc33dbd4bb46ba2d7db8a423c8052bce4d16d17
Subhrajoti Chakrabarty <148115965+Subhrajoti@users.noreply.github.com>
2024-03-05 22:40:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello Vistors");
+console.log("Hello Every Vistors");
 // small JavaScript Project:
 var img = document.getElementById("puppyImage");
 var button = document.getElementById("likeButton");
